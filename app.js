dd6c1e6eda67d0bf6b83fe6fdad3c62f8c3f3428
Kshitiz gupta <66310076+Kshitiz-code@users.noreply.github.com>
2022-05-19 16:44:30 +0530

    js update

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
-let equal_pressed = 0;
-let button_input = document.querySelectorAll(".input-button");
-let input = document.getElementById("input");
-let equal = document.getElementById("equal");
-let clear = document.getElementById("clear");
-let erase = document.getElementById("erase");
+var password=document.getElementById("password");
+    function genPassword() {
+        var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var passwordLength = 12;
+        var password = "";
+        for (var i = 0; i <= passwordLength; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber +1);
+        }
+        document.getElementById("password").value = password;
 
-window.onload = () => {
-  input.value = "";
-};
-
-button_input.forEach((button_class) => {
-  button_class.addEventListener("click", () => {
-    if (equal_pressed == 1) {
-      input.value = "";
-      equal_pressed = 0;
     }
-    input.value += button_class.value;
-  });
-});
-
-equal.addEventListener("click", () => {
-  equal_pressed = 1;
-  let inp_val = input.value;
-  try {
-    let solution = eval(inp_val);
-    if (Number.isInteger(solution)) {
-      input.value = solution;
-    } else {
-      input.value = solution.toFixed(2);
-    }
-  } catch (err) {
-    alert("Invalid Input");
-  }
-});
-
-clear.addEventListener("click", () => {
-  input.value = "";
-});
-
-erase.addEventListener("click", () => {
-  input.value = input.value.substr(0, input.value.length - 1);
-});
+    function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0, 999);
+  document.execCommand("copy");
+  
+}
